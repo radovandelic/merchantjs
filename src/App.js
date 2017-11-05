e71fec0995accdc23be0a84b617607100f858694
ivoryblakk <george.ingram1@outlook.com>
2017-11-05 21:15:42 +0100

    first commit George

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, Quantity, Materials } from './interface';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Medival Villge</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, choose what material to buy
+          <Materials /><Quantity /><Button />
         </p>
       </div>
     );
