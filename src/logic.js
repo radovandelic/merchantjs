e86448c34dd21b442f5735ad78180a7d34301228
Radovan Delic <infamouszgbgd@yahoo.com>
2017-11-05 20:57:32 +0100

    logic.js

---
+++ src/logic.js
@@ -0,0 +1,22 @@
+var logic = {
+    prices: {
+        wood: 2.0,
+        stone: 1.0,
+        metal: 5.0
+    },
+    getNewPrice: function (commodity) {
+        price = this.prices[commodity];
+        price = price + (Math.random() * price / 4 - Math.random() * price / 4)
+        this.prices[commodity] = price;
+        return +price.toFixed(2);
+    },
+    getPrice: function (commodity) {
+        return this.prices[commodity];
+    },
+    getAll: function () {
+        return prices;
+    },
+    setPrice: function (commodity, price) {
+        this.prices[commodity] = price;
+    }
+}
